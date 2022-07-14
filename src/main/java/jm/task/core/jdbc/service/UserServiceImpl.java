63f90aa42c406eb71ad3f3bd409cfceafd74de76
Alex Dobrovolski <triiq@mail.ru>
2022-07-14 14:59:29 +0300

    UserServiceImpl: add private final variable for users, override all methods

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,40 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private final UserDaoJDBCImpl users = new UserDaoJDBCImpl();
+
+    @Override
     public void createUsersTable() {
-
+        users.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
-
+        users.dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        users.saveUser(name, lastName, age);
     }
 
+    @Override
     public void removeUserById(long id) {
-
+        users.removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        return users.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
-
+        users.cleanUsersTable();
     }
 }
