53140fd2ec78b13a26033fff2d72a5d023ec8074
Alex Dobrovolski <triiq@mail.ru>
2022-07-14 15:11:45 +0300

    Main: add algorithm of the program

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService users = new UserServiceImpl();
+
+        users.dropUsersTable();
+        users.createUsersTable();
+
+        users.saveUser("Ivan", "Dow", (byte) 3);
+        users.saveUser("Tony", "Smith", (byte) 22);
+        users.saveUser("Catherine", "James", (byte) 18);
+        users.saveUser("Matthew", "Brown", (byte) 107);
+
+        System.out.println(users.getAllUsers());
+
+        users.cleanUsersTable();
+        users.dropUsersTable();
     }
 }
