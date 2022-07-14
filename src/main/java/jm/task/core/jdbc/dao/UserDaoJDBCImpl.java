9c6643847cf7a4e8b0aaaa7c46145b26c20aef12
Alex Dobrovolski <triiq@mail.ru>
2022-07-14 15:00:19 +0300

    UserDaoJDBCImpl: override all methods

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,110 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private final String tableName = "users";
+    private final String schemaName = "preproject";
+
     public UserDaoJDBCImpl() {
-
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute("CREATE SCHEMA IF NOT EXISTS " + schemaName + ";");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    @Override
     public void createUsersTable() {
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute("USE " + schemaName + ";");
+            statement.execute("CREATE TABLE IF NOT EXISTS " + tableName +
+                    "(id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+                    "name VARCHAR(45), " +
+                    "last_name VARCHAR(45), " +
+                    "age TINYINT);");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
+    @Override
     public void dropUsersTable() {
-
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute("USE " + schemaName + ";");
+            statement.execute("DROP TABLE IF EXISTS " + tableName + ";");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute("USE " + schemaName + ";");
+            statement.execute("INSERT INTO " + tableName +
+                    "(name, last_name, age) VALUES ('" + name + "', '" + lastName + "', " + age + ");");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    @Override
     public void removeUserById(long id) {
-
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute("USE " + schemaName + ";");
+            statement.execute("DELETE FROM " + tableName + " WHERE id=" + id + ";");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute("USE " + schemaName + ";");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM " + tableName + ";");
+
+            while (resultSet.next()) {
+                User user = new User(resultSet.getString("name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getByte("age"));
+                user.setId(resultSet.getLong("id"));
+
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return users;
     }
 
+    @Override
     public void cleanUsersTable() {
-
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute("USE " + schemaName + ";");
+            statement.execute("DELETE FROM " + tableName + ";");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
