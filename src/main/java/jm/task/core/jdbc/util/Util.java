d67df07fe682261e0068afe6e47f09a2d2e67c63
Alex Dobrovolski <triiq@mail.ru>
2022-07-14 09:00:43 +0300

    Util: add getConnection()

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+        Connection connection;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return connection;
+    }
 }
