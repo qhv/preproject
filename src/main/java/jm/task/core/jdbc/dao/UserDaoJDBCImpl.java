302c11d64fa808a8bda9f9c28957a90a65866a01
Alex Dobrovolski <triiq@mail.ru>
2022-07-18 09:12:46 +0300

    UserDaoJDBCImpl: rewriting sql queries

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,13 +8,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final String schemaName = "preproject";
-    private final String tableName = schemaName + ".users";
 
     public UserDaoJDBCImpl() {
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement()) {
-            statement.execute("CREATE SCHEMA IF NOT EXISTS " + schemaName + ";");
+            statement.execute("CREATE SCHEMA IF NOT EXISTS preproject;");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -24,11 +22,14 @@ public class UserDaoJDBCImpl implements UserDao {
     public void createUsersTable() {
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement()) {
-            statement.execute("CREATE TABLE IF NOT EXISTS " + tableName +
-                    "(id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
-                    "name VARCHAR(45), " +
-                    "last_name VARCHAR(45), " +
-                    "age TINYINT);");
+
+            statement.execute("""
+                    CREATE TABLE IF NOT EXISTS preproject.users
+                    (id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                    name VARCHAR(45),
+                    last_name VARCHAR(45),
+                    age TINYINT);
+                    """);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -39,7 +40,7 @@ public class UserDaoJDBCImpl implements UserDao {
     public void dropUsersTable() {
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement()) {
-            statement.execute("DROP TABLE IF EXISTS " + tableName + ";");
+            statement.execute("DROP TABLE IF EXISTS preproject.users;");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -49,7 +50,7 @@ public class UserDaoJDBCImpl implements UserDao {
     public void saveUser(String name, String lastName, byte age) {
         try (Connection connection = Util.getConnection();
              PreparedStatement ps = connection.prepareStatement(
-                     "INSERT INTO " + tableName + "(name, last_name, age) VALUES (?, ?, ?);")) {
+                     "INSERT INTO preproject.users(name, last_name, age) VALUES (?, ?, ?);")) {
             ps.setString(1, name);
             ps.setString(2, lastName);
             ps.setByte(3, age);
@@ -63,7 +64,7 @@ public class UserDaoJDBCImpl implements UserDao {
     @Override
     public void removeUserById(long id) {
         try (Connection connection = Util.getConnection();
-             PreparedStatement ps = connection.prepareStatement("DELETE FROM " + tableName + " WHERE id=?;")) {
+             PreparedStatement ps = connection.prepareStatement("DELETE FROM preproject.users WHERE id=?;")) {
             ps.setLong(1, id);
 
             ps.executeUpdate();
@@ -78,7 +79,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT * FROM " + tableName + ";")) {
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM preproject.users;")) {
 
             while (resultSet.next()) {
                 users.add(new User(resultSet.getLong("id"),
@@ -97,7 +98,7 @@ public class UserDaoJDBCImpl implements UserDao {
     public void cleanUsersTable() {
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement()) {
-            statement.execute("DELETE FROM " + tableName + ";");
+            statement.execute("DELETE FROM preproject.users;");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
