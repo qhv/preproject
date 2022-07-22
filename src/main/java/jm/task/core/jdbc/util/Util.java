19f8e457b141ca5b72f394d3b73781a92ec8681a
Alex Dobrovolski <triiq@mail.ru>
2022-07-22 11:28:00 +0300

    Util: add JDBC functions: openConnection() and closeConnection()

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,19 +1,86 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Util {
-    public static Connection getConnection() {
-        Connection connection;
+
+    private final static String URL = "jdbc:mysql://0.0.0.0:55002";
+    private final static String USER_NAME = "pp_user";
+    private final static String PASSWORD = "pa$$";
+
+    private static Connection connection;
+
+    /**
+     * JDBC connection functions
+     */
+    public static void openConnection() {
+        if (connection != null) return;
 
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "");
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static Connection getConnection() {
         return connection;
+    }
+
+    /**
+     *  Hibernate connection functions
+     */
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory != null) return sessionFactory;
+
+        try {
+            Map<String, Object> settings = new HashMap<>();
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, URL);
+            settings.put(Environment.USER, USER_NAME);
+            settings.put(Environment.PASS, PASSWORD);
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+
+            StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+            registryBuilder.applySettings(settings);
+            StandardServiceRegistry registry = registryBuilder.build();
+
+            MetadataSources sources = new MetadataSources(registry);
+            sources.addAnnotatedClass(User.class);
+
+            Metadata metadata = sources.getMetadataBuilder().build();
+
+            sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+            StandardServiceRegistryBuilder.destroy(registry);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return sessionFactory;
+
     }
 }
