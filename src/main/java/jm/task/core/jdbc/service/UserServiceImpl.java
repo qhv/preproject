1c914506d89aa8346dae90116f425ed04c0a069d
Alex Dobrovolski <triiq@mail.ru>
2022-07-23 10:37:44 +0300

    UserServiceImpl: rewrite users field

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -6,7 +6,7 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDaoJDBCImpl users = new UserDaoJDBCImpl();
+    private final UserDaoJDBCImpl users = UserDaoJDBCImpl.getInstance();
 
     @Override
     public void createUsersTable() {
